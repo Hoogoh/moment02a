421fb2fb1908b3970d5e2de23943a44dab4e0e8c
Hoogoh <98016405+Hoogoh@users.noreply.github.com>
2022-09-26 20:18:56 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Hugo Fagerström, Tk21
+# Moment02a
+
+r = float(input("Skriv radien av cirkeln i cm: ")) #Här gör jag så att man kan skriva in radien av cirkeln
+
+pi=3.14
+
+omkrets=2*pi*r
+
+area=pi*r*r
+#Formlerna för hur man räknar ut en cirkels area och omkrets
+
+print("Area av cirkeln: ",f"{area:5.2f}" "cm\u00b2")     # \u00b2 gör så att svaret blir kvadrat
+
+print("Omkretsen av cirkeln: ",f"{omkrets:5.2f}" "cm")
